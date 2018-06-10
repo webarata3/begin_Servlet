267ef51a98523d046347ff5b02ac811a24580d19
webarata3 <webarata3@gmail.com>
2018-06-10 14:46:27 +0900

    begin04

---
+++ begin04/src/servlet/PostTestServlet.java
@@ -0,0 +1,33 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/post-test")
+public class PostTestServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.setCharacterEncoding(StandardCharsets.UTF_8.name());
+        String param1 = request.getParameter("param1");
+        String param2 = request.getParameter("param2");
+
+        response.setContentType("text/html");
+        response.setCharacterEncoding(StandardCharsets.UTF_8.name());
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<html><head><meta charset=\"utf-8\"></head><body>");
+            out.println("param1=" + param1 + "<br>");
+            out.println("param2=" + param2 + "<br>");
+            out.println("</body></html>");
+        }
+    }
+}
